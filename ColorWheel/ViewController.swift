a60e43aefd422a302ff75dcc24363f001f9267df
Alwyn Yeo <alwynyeo98@gmail.com>
2024-08-15 20:17:38 +0800

    Initial Commit

---
+++ ColorWheel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorWheel
+//
+//  Created by Alwyn Yeo on 8/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ColorWheelUITests/ColorWheelUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ColorWheelUITestsLaunchTests.swift
+//  ColorWheelUITests
+//
+//  Created by Alwyn Yeo on 8/15/24.
+//
+
+import XCTest
+
+final class ColorWheelUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
